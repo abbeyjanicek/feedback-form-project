d5ee68ebf4d42e055859ff8ef5c6b9bffb712b46
Abbey Janicek <abbey.janicek@gmail.com>
2018-09-09 08:47:46 -0500

    Set-up store and provider. Setting up routes.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+// import Header from '../Header/Header.js'
+// import FeelingView from '../FeelingView/FeelingView.js';
+// import UnderstandingView from '../UnderstandingView/UnderstandingView.js';
+// import SupportView from '../SupportView/SupportView.js';
+// import CommentsView from '../CommentsView/CommentsView.js';
+// import SubmissionSuccessPage from '../SubmissionSuccessPage/SubmissionSuccessPage.js';
+// import AdminView from '../AdminView/AdminView.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
+        {/* <Header />
+        <Route path="/feeling" component={FeelingView} />
+        <Route path="/understanding" component={UnderstandingView} />
+        <Route path="/support" component={SupportView} />
+        <Route path="/comments" component={CommentsView} />
+        <Route path="/submission" component={SubmissionSuccessPage} />
+        <Route path="/admin" component={AdminView} /> */}
         <br/>
       </div>
     );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+<header className="App-header">
+          <h1 className="App-title">Feedback!</h1>
+          <h4><i>Don't forget it!</i></h4>
+        </header>
