fb5aac6c23f9718a1a973e12eecc9ec3cc9e88c3
Abbey Janicek <abbey.janicek@gmail.com>
2018-09-09 09:15:18 -0500

    Created feedbackForm reducer.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, bindActionCreators } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
-const feedbackInput = '';
+const feedbackInput = {
+    feeling: '',
+    understanding: '',
+    support: '',
+    comments: '',
+}
 
-const feedbackForm = (state = feedbackInput) => {
+const feedbackForm = (state = feedbackInput, action) => {
+    if (action.type === 'ADD_FEEDBACK') {
+        const feedback = action.payload;
+        return {...state, feedback }
+    }
     return state;
 }
 
